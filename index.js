a4bfcc4ec7871b0c95804f30ebaee797a5ecb9e4
Móricz János Milán <20g_moriczj@NYIRSZIKSZI.local>
2021-10-07 13:34:21 +0200

    feltöltés

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 const http = require('http');
-const port=4444;
+const port= 4444;
 const fs = require('fs');
 
 
 const server = http.createServer((req, res) => {
-    res.write(`A kérés forrása: ${req.url}`);
-    res.end('   -Vége');
+    console.log(req.url);
+    console.log(req.method);
+    
+
+    //router
+    switch(true){
+        case req.url === '/' && req.method === 'GET':
+            fs.readFile('./views/index.html', (err,file) => {
+                res.setHeader('Content-type', 'text/html');
+                res.end(file);
+            });
+            break;
+            case req.url === '/minta.css' && req.method === 'GET':
+            fs.readFile('./views/minta2.css', (err,file) => {
+                res.setHeader('Content-type', 'text/css');
+                res.end(file);
+            });
+            break;
+            case req.url === '/telefon' && req.method === 'GET':
+                kiszolgal('./views/telefon.json', 'application/json', req, res);
+                break;
+                default:
+            kiszolgal('./views/hiba404.html', 'text/html', req, res);
+            //majd ezt is kuldom allomanybol
+
+            //res.end('ezt válaszoltam');
+    }
 })
 
-
+function kiszolgal(filename, type, req, res){
+    fs.readFile(filename, (err,file) => {
+        res.setHeader('Content-type', 'text/html');
+        res.end(file);
+    });
+}
 console.log('megy')
 server.listen(port);
